f384fe85821a4cb8ac6256d76ea3c8b2577f6626
faisal-hossain1429 <121063890+faisal-hossain1429@users.noreply.github.com>
2023-08-10 00:21:45 -0400

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,82 @@
+﻿using FizzBuzzMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace FizzBuzzMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult FBPage()
+        {
+            FizzBuzz model = new();
+
+            model.FizzValue = 3;
+            model.BuzzValue = 5;
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult FBPage(FizzBuzz fizzbuzz)
+        {
+            List<string> fbitems = new();
+
+            bool fizz;
+            bool buzz;
+
+            for (int i = 1; i <= 100; i++)
+            {
+                fizz = (i % fizzbuzz.FizzValue == 0);
+                buzz = (i % fizzbuzz.BuzzValue == 0);
+
+                if (fizz == true && buzz == true)
+                {
+                    fbitems.Add("FizzBuzz");
+                }
+                else if (fizz == true)
+                {
+                    fbitems.Add("Fizz");
+                }
+                else if (buzz == true)
+                {
+                    fbitems.Add("Buzz");
+                }
+                else
+                {
+                    fbitems.Add(i.ToString());
+                }
+            }
+
+            fizzbuzz.Result = fbitems;  
+
+            return View(fizzbuzz);
+        }
+
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
